8c638a7d40f351f063386de43f12ef245752c091
JakubKozak <jakub.kozak15@seznam.cz>
2020-11-04 18:16:16 +0000

    diskriminant, prvocisla

--- script.js
+++ script.js
@@ -5,17 +5,15 @@ function kvadr() {
 
   let d = (b*b)-(4*a*c);
 
+  document.getElementById("Disk").innerHTML=d;
   if (d < 0){
-    document.getElementById("Disk").innerHTML=d;
-    document.getElementById("x1").innerHTML="rce nemá řešení";
-    document.getElementById("x2").innerHTML="rce nemá řešení";
+    document.getElementById("x1").innerHTML="rovnice nemá řešení";
+    document.getElementById("x2").innerHTML="rovnice nemá řešení";
   } else if (d == 0){
-    document.getElementById("Disk").innerHTML=d;
     x1 = -b/(2*a)
     document.getElementById("x1").innerHTML=x1;
     document.getElementById("x2").innerHTML="schodné s x1";
   } else {
-    document.getElementById("Disk").innerHTML=d;
     x1 = (-b + Math.sqrt(d))/(2*a);
     x2 = (-b - Math.sqrt(d))/(2*a);
     document.getElementById("x1").innerHTML=x1;
@@ -46,12 +44,10 @@ function prvocisla() {
   let num = 1;
 
   while (count < pocet) {
+    num = num + 1;
     if (P(num)) {
       vypis = vypis + num + " ";
-      num = num + 1
       count = count + 1;
-    } else {
-      num = num + 1;
     }
   }
   document.getElementById("vypis").innerHTML = vypis;
